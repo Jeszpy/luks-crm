fd02a43a7fb636178eacce87b539c064b4c2eb9d
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-05 06:12:01 +0300

    fix: return class init values because if you do not pass the values - the transform will not work

--- apps/server/src/helpers/pagination/default.pagination.ts
+++ apps/server/src/helpers/pagination/default.pagination.ts
@@ -2,9 +2,10 @@ import { IsInteger } from '../../decorators/is-integer.decorator';
 
 export class DefaultPagination {
   @IsInteger(1, 1)
-  public pageNumber: number;
+  public pageNumber = 1;
+
   @IsInteger(10, 1)
-  public pageSize: number;
+  public pageSize = 10;
 
   public skip() {
     return (this.pageNumber - 1) * this.pageSize;
