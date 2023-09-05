63f5be894b3317a6b87aa18d9b536623d9b5f4b1
Jeszpy <hleb.lukashonak@gmail.com>
2023-09-05 04:13:03 +0300

    ref: check api connection

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './modules/database/database.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello New World!';
+  constructor(private readonly prisma: DatabaseService) {}
+  async getHello() {
+    const newUser = await this.prisma.user.create({
+      data: { login: 'new login', password: '123' },
+    });
+    console.log(newUser);
+    return newUser.login;
   }
 }
